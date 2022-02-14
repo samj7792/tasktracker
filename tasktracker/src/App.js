97be3779f6b07720390b3508d16b5bb74fe9f1e6
Sam Jackson <samuelsjackson7@gmail.com>
2022-02-14 14:00:00 -0500

    delete task functionality

--- tasktracker/src/App.js
+++ tasktracker/src/App.js
@@ -24,10 +24,19 @@ function App() {
     },
   ]);
 
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className='container'>
       <Header title='Task Tracker' />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        'No Tasks'
+      )}
     </div>
   );
 }
